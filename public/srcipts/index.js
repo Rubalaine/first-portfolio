2e573721a0ca7c04f5747db79ce12f92c7f8ddee
Rubalaine <kelvenizzy@gmail.com>
2020-10-29 19:35:17 +0200

    text and image added to the header

--- public/srcipts/index.js
+++ public/srcipts/index.js
@@ -1,6 +1,21 @@
 const showNav = document.getElementById("showNav");
 const mobileNav = document.getElementById("mobileNav");
 const darkMode = document.getElementById("dark-switch");
+
+const text =
+  "Olá eu sou KELVEN RUBALAINE Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi modi quod aspernatur omnis illum minima repudiandae et beatae aut porro, in uas qui. Corrupti minus deleniti quisquam quia iusto.";
+let iterator = 0;
+const startTypyng = () => {
+  if (iterator < text.length) {
+    document.getElementById("typewriter").innerHTML += text.charAt(iterator);
+    iterator++;
+    setTimeout(startTypyng, 50);
+  }
+};
+document.addEventListener("DOMContentLoaded", (event) => {
+  event.preventDefault();
+  startTypyng();
+});
 if (showNav)
   showNav.addEventListener("click", (event) => {
     event.preventDefault();
