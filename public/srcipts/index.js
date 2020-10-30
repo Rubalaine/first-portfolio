a9b5ac37665d5156768b4db036b34a068dfe41c5
Rubalaine <kelvenizzy@gmail.com>
2020-10-30 07:54:47 +0200

    now can add dark mode state to the localstore

--- public/srcipts/index.js
+++ public/srcipts/index.js
@@ -2,19 +2,21 @@ const showNav = document.getElementById("showNav");
 const mobileNav = document.getElementById("mobileNav");
 const darkMode = document.getElementById("dark-switch");
 
-const text =
-  "Olá eu sou KELVEN RUBALAINE Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi modi quod aspernatur omnis illum minima repudiandae et beatae aut porro, in uas qui. Corrupti minus deleniti quisquam quia iusto.";
-let iterator = 0;
-const startTypyng = () => {
-  if (iterator < text.length) {
-    document.getElementById("typewriter").innerHTML += text.charAt(iterator);
-    iterator++;
-    setTimeout(startTypyng, 50);
-  }
-};
+// const text =
+//   "Olá eu sou KELVEN RUBALAINE Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi modi quod aspernatur omnis illum minima repudiandae et beatae aut porro, in uas qui. Corrupti minus deleniti quisquam quia iusto.";
+// let iterator = 0;
+// const startTypyng = () => {
+//   if (iterator < text.length) {
+//     document.getElementById("typewriter").innerHTML += text.charAt(iterator);
+//     iterator++;
+//     setTimeout(startTypyng, 50);
+//   }
+// };
 document.addEventListener("DOMContentLoaded", (event) => {
   event.preventDefault();
-  startTypyng();
+  document.body.classList.add(localStorage.theme || "s");
+  document.getElementById("sun").classList.remove(localStorage.sun || "s");
+  document.getElementById("moon").classList.add(localStorage.sun || "s");
 });
 if (showNav)
   showNav.addEventListener("click", (event) => {
@@ -28,4 +30,11 @@ if (darkMode)
     document.getElementById("moon").classList.toggle("hidden");
     document.getElementById("sun").classList.toggle("hidden");
     document.body.classList.toggle("dark");
+    localStorage.setItem("theme", "dark");
+    localStorage.setItem("sun", "hidden");
+
+    if (!document.body.classList.contains("dark")) {
+      localStorage.removeItem("theme");
+      localStorage.removeItem("sun");
+    }
   });
